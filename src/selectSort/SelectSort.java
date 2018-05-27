1a0e70f9bc4397b050b4b8f00f6f38eff9c7d099
liupfde <liupfde@163.com>
2018-05-27 15:17:12 +0800

    解释

--- src/selectSort/SelectSort.java
+++ src/selectSort/SelectSort.java
@@ -2,6 +2,8 @@ package selectSort;
 /**
  * 简单的选择排序
  * author liupf
+ *
+ * 多次比较，一次交换
  */
 public class SelectSort {
     public static void main(String args[]){
@@ -16,12 +18,13 @@ public class SelectSort {
         for(int i=0;i<length;i++){
 
             min = i;//当前最小值的下标
+            //先进行多次比较，找出最小的值
             for(int j = i+1;j<length;j++){
                 if (s[j]<s[min]) {
-                    min = j;
+                    min = j;//将本次比较最小值的下标赋给min
                 }
             }
-
+            //交换数据
             if(i<min){
                 int temp = s[i];
                 s[i] = s[min];
